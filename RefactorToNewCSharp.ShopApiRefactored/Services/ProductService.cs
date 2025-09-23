1b07353396659767b0669e1105433e4c45c6d287
Alireza <a.z.nikravesh@gmail.comm>
2025-09-23 19:55:34 +0330

    refactored productservice

--- RefactorToNewCSharp.ShopApiRefactored/Services/ProductService.cs
+++ RefactorToNewCSharp.ShopApiRefactored/Services/ProductService.cs
@@ -3,19 +3,23 @@
 using ShopApp.Data;
 
 namespace ShopApp.Services;
-public class ProductService (ShopDbContext db)
+public class ProductService(ShopDbContext db)
 {
-    public void AddProduct(Product product)
-    {
-        db.Products.Add(product);
-        db.SaveChanges();
-    }
+    private const int CheapCost = 100;
+    private const int NormalCost = 1000;
+    public void AddProduct(Product product) => db.Add(product);
+
+    public async Task AddProductAsync(Product product) => await db.AddAsync(product);
+
+    public void Save() => db.SaveChanges();
+
+    public Task SaveAsync() => db.SaveChangesAsync();
 
     public void CategorizeProduct(Product product)
     {
-        if (product.Price < 100)
+        if (product.Price < CheapCost)
             Console.WriteLine($"{product.Name} is Cheap");
-        else if (product.Price >= 100 && product.Price < 1000)
+        else if (product.Price >= CheapCost && product.Price < NormalCost)
             Console.WriteLine($"{product.Name} is Normal");
         else
             Console.WriteLine($"{product.Name} is Expensive");
