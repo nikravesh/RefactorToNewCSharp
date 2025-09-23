0f8c7f7cf45271d39ba36fa2e2df004cfad17cf6
Alireza <a.z.nikravesh@gmail.comm>
2025-09-23 20:09:17 +0330

    added logging interceptor

---
+++ RefactorToNewCSharp.ShopApiRefactored/Data/LoggingInterceptor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+using RefactorToNewCSharp.ShopApiRefactored.Models;
+
+namespace RefactorToNewCSharp.ShopApiRefactored.Data;
+public class LoggingInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData,
+        InterceptionResult<int> result)
+    {
+        var context = eventData.Context;
+
+        if (context is not null)
+        {
+            var entiries = context.ChangeTracker.Entries<Product>().Where(p => p.State == EntityState.Added);
+
+            foreach (var entry in entiries)
+            {
+                Console.WriteLine($@"[Information Log] : 
+                                     Adding product : {entry.Entity.Name}, Price : {entry.Entity.Price}");
+            }
+        }
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        var context = eventData.Context;
+
+        if (context is not null)
+        {
+            var entries = context.ChangeTracker.Entries<Product>().Where(p => p.State == EntityState.Added);
+
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($@"[Information Log(async method)] : 
+                                   Adding product : {entry.Entity.Name}, Price : {entry.Entity.Price}");
+            }
+        }
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
