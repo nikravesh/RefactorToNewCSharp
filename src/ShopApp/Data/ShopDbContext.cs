052d6c5d8624a21efa3605bb73acc7c1c1b04bac
Alireza Nikravesh <a.z.nikravesh@gmail.com>
2025-09-23 15:15:01 +0330

    added configuration for EF

--- src/ShopApp/Data/ShopDbContext.cs
+++ src/ShopApp/Data/ShopDbContext.cs
@@ -6,4 +6,11 @@ namespace ShopApp.Data;
 public class ShopDbContext : DbContext
 {
     public DbSet<Product> Products {  get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseInMemoryDatabase("ShopDb")
+            .AddInterceptors(new LoggingInterceptor());
+    }
 }
