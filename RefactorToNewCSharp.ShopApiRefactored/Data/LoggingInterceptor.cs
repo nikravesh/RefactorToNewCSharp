b70caf4aede6f6da510fe02e04c6ef50221e9da1
Alireza <a.z.nikravesh@gmail.comm>
2025-09-23 21:07:02 +0330

    added shadowproperty in interceptor

--- RefactorToNewCSharp.ShopApiRefactored/Data/LoggingInterceptor.cs
+++ RefactorToNewCSharp.ShopApiRefactored/Data/LoggingInterceptor.cs
@@ -6,6 +6,7 @@ using RefactorToNewCSharp.ShopApiRefactored.Models;
 namespace RefactorToNewCSharp.ShopApiRefactored.Data;
 public class LoggingInterceptor : SaveChangesInterceptor
 {
+    private int userId = 1;
     public override InterceptionResult<int> SavingChanges(
         DbContextEventData eventData,
         InterceptionResult<int> result)
@@ -19,6 +20,12 @@ public class LoggingInterceptor : SaveChangesInterceptor
             foreach (var entry in entiries)
             {
                 Console.WriteLine($@"[Information Log]: Adding product : {entry.Entity.Name}, Price: {entry.Entity.Price}");
+
+                entry.Property("CreatedAt").CurrentValue = DateTime.Now;
+                entry.Property("CreatedBy").CurrentValue = userId;
+
+                Console.WriteLine($"Adding time: {entry.Property("CreatedAt").CurrentValue}");
+                Console.WriteLine($"Adding by: {entry.Property("CreatedBy").CurrentValue}");
             }
         }
 
@@ -39,6 +46,12 @@ public class LoggingInterceptor : SaveChangesInterceptor
             foreach (var entry in entries)
             {
                 Console.WriteLine($@"[Information Log(async method)]: Adding product : {entry.Entity.Name}, Price: {entry.Entity.Price}");
+
+                entry.Property("CreatedAt").CurrentValue = DateTime.Now;
+                entry.Property("CreatedBy").CurrentValue = userId;
+
+                Console.WriteLine($"Adding time: {entry.Property("CreatedAt").CurrentValue}");
+                Console.WriteLine($"Adding by: {entry.Property("CreatedBy").CurrentValue}");
             }
         }
         return base.SavingChangesAsync(eventData, result, cancellationToken);
--- RefactorToNewCSharp.ShopApiRefactored/Data/ShopDbContext.cs
+++ RefactorToNewCSharp.ShopApiRefactored/Data/ShopDbContext.cs
@@ -7,6 +7,14 @@ public class ShopDbContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().Property<DateTime>("CreatedAt");
+        modelBuilder.Entity<Product>().Property<int>("CreatedBy");
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseInMemoryDatabase("ShopDb")
